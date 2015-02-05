4a36f96a7c7bed13f34ab5cd69980be6506d0f19
DrMambo <fillion.brian@gmail.com>
2015-02-04 21:14:00 -0500

    sa build weee

--- main.py
+++ main.py
@@ -17,32 +17,37 @@ def getFileData():
 
 # Fonction récursive, prend en parametre p, q et le numéro de génération
 def getElfGeneration(p, q, generation):
+    print(str(p)+"/"+str(q) + " "+str(generation))
     # Si on dépasse 40 en génération, on a failé
     if (generation > 40):
         return -1
     
     # Si on a un elfe, on a la bonne génération
-    elif (p/q == 1):
+    elif (int(p)/int(q) == 1):
         return generation
 
     # Sinon, on essaie de tendre vers 1
     else:
-        return getElfGeneration(2*p, q, generation + 1)
+        return getElfGeneration(str(2*int(p)), q, generation + 1)
+
+
 
 def main():
-    data = getFileData()
-    
-    for i in range(len(data)):
+    #data = getFileData()
+    data = open(files[0]).read().splitlines()
+    amountTest = data[0]
+    print(str(len(data)))
+    for i in range(1,len(data)):
         case = data[i]
         p, q = case.split('/')
-        
+        print("why me?")
         # Démarrer la récursion
         gen = getElfGeneration(p, q, 1)
-        
+
         if (gen == -1):
             gen = "impossible"
-        
-        print ("Case #" + i + ": " + gen)
+
+        print ("Case #" + str(i) + ": " + str(gen))
     
     
 if __name__ == "__main__":
